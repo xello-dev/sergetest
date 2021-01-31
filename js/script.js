97a1b4d62948e1e843ae9c3a8e5c09f8ba22a5c7
Serge Balashov <astrakat@mail.ru>
2021-01-31 14:28:56 +0400

    first

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// const answers = [];
+
+// answers[0] = prompt("Ваше имя?", "");
+// answers[1] = prompt("Ваша  фамилия?", "");
+// answers[2] = prompt("Сколько вам лет?", "");
+
+// document.write(answers);
+
+// const user = 'Serge';
+// alert(`ПРивет,${user}`);
+
+incr++;
+decr--;
